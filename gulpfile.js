44a22875e1e83a8fe133262b535497d081efdcc4
Lopi <lototchi@redkiwi.nl>
2016-08-10 15:55:12 +0300

    crush json

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
 	minifyHTML = require('gulp-minify-html'),
+	jsonminify = require('gulp-jsonminify'),
 	concat = require('gulp-concat');
 	
 var env,
@@ -75,6 +76,7 @@ gulp.task('watch', function(){
 	gulp.watch(jsSources, ['js'])
 	gulp.watch('components/sass/*.scss', ['compass'])
 	gulp.watch('builds/development/*.html', ['html']);
+	gulp.watch('builds/development/js/*.json', ['json']);
 	gulp.watch(htmlSources, ['json']);
 });
 
@@ -93,7 +95,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('json', function() {
-	gulp.src(outputDir + '/js/*.json')
+	gulp.src('builds/development/js/*.json')
+		.pipe(gulpif(env === 'production', jsonminify() ))
+		.pipe(gulpif(env === 'production', gulp.dest('builds/production/js') ))
 		.pipe(connect.reload())
 });
 
